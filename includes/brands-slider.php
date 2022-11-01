ddfabb6fdf14ec8f07a49aaeebb507475ede94fa
farobledo <fabian.robledolave@gmail.com>
2022-10-31 21:39:09 -0500

    Opticavisual

---
+++ includes/brands-slider.php
@@ -0,0 +1,104 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Marcas Reconocidas</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/gafas1.jpg" src="assets/images/gafas1.jpg" alt="">
+						<h5 class="titulo">Tiwi</h5>
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/gafas-sol.jpg" src="assets/images/gafas1.jpg" alt="">
+						<h5 class="titulo">CELINE HOMME</h5>
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/gafasol.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">EMPORIO ARMANI</h5>
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/guci.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">GUCCI</h5>
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/start.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">STARLITE</h5>
+					</a>	
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/vogue.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">VOGUE</h5>
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/jack-jones.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">JACK & JONES</h5>
+					</a>	
+				</div>
+<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/australia.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">QUAY AUSTRALIA</h5>
+					</a>	
+				</div>
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/zipizi.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">IZIPIZI</h5>
+					</a>	
+				</div>
+
+				
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/oliver.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">OLIVER PEOPLES</h5>
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/polaroid.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">POLAROID</h5>
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/cherri.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">SERIFF & CHERRY</h5>
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="img">
+						<img data-echo="brandsimage/tornasol.jpg" src="assets/images/blank.gif" alt="">
+						<h5 class="titulo">OAKLEY</h5>
+					</a>	
+				</div>
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+	</div><!-- /.logo-slider-inner -->
+	
+</div>
+<!-- /.logo-slider -->
